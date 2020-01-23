220926d2fd93cc6ad39c1d1b0b2b0ec6d0bbeca8
Riku Ruokonen <riku.ruokonen@alten.fi>
2020-01-23 14:11:49 +0200

    modified texts and added tests

--- tests/routes.spec.js
+++ tests/routes.spec.js
@@ -1,25 +1,77 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const { checkForWinner } = require('../game/gameLogic');
-const { startGame, executeMove, reset } = require('../routes/gameRoutes');
+const texts = require('../resources/texts.json');
 const app = require('../index');
+
+const { expect } = chai;
 
 
 chai.use(chaiHttp);
 
-/* describe('Game-routes', () => {
+describe('Game-routes', () => {
   describe('/GET at start', () => {
-    it('should start the game, and return text to telling so', (done) => {
+    it('should start the game, and return 200 and corresponding message', (done) => {
       chai.request(app)
-        .get('/start')
+        .get('/api/start')
         .end((err, res) => {
           if (err) {
             done(err);
           }
-          console.log(res);
           expect(res).to.have.status(200);
+          expect(res.body.message).to.be.oneOf([texts.playerStarts, texts.computerStarts]);
+          done();
+        });
+    });
+  });
+
+  describe('/POST at do-move', () => {
+    it('should execute move and return 200 and corresponding message', (done) => {
+      chai.request(app)
+        .post('/api/do-move')
+        .send({
+          move: {
+            row: 1,
+            column: 1,
+            playerNumber: 1,
+          },
+        })
+        .end((err, res) => {
+          if (err) {
+            done(err);
+          }
+          expect(res).to.have.status(200);
+          expect(res.body.updatedBoard[1][1]).to.equal(1);
+          expect(res.body.message).to.equal(texts.yourTurn);
+          done();
+        });
+    });
+  });
+  describe('/GET at status', () => {
+    it('should get status of current game and return 200', (done) => {
+      chai.request(app)
+        .get('/api/status')
+        .end((err, res) => {
+          if (err) {
+            done(err);
+          }
+          expect(res).to.have.status(200);
+          expect(res.body.board[1][1]).to.equal(1);
+          done();
+        });
+    });
+  });
+  describe('/GET at reset', () => {
+    it('should reset the status of current game and return 200', (done) => {
+      chai.request(app)
+        .get('/api/reset')
+        .end((err, res) => {
+          if (err) {
+            done(err);
+          }
+          expect(res).to.have.status(200);
+          expect(res.body.message).to.be.oneOf([texts.playerStarts, texts.computerStarts]);
+          done();
         });
     });
   });
 });
- */
